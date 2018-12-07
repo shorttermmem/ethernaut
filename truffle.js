e0ab2c327950c5e39bfac12f99f2a6f98ca25494
mike <freshprogrammers@gmail.com>
2018-12-06 23:53:25 -0500

    DEV - add configure files for EPM, VSCode, NPM and update solidity compiler, mocha option

--- truffle.js
+++ truffle.js
@@ -1,30 +1,12 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
-
- /*
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
-*/
-var HDWalletProvider = require("truffle-hdwallet-provider");
+//https://truffleframework.com/docs/truffle/reference/configuration
+let HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
+  //contracts_build_directory: "./build",
   networks: {
     development: {
       host: "localhost",
-      port: 9545,     // debugger expects to be on port 9545
+      port: 9545,
       network_id: "*" // Match any network id
     },
     ganache: {
@@ -39,7 +21,23 @@ module.exports = {
           "https://ropsten.infura.io/db29611c34844e2197634a44dbac1344")
       },
       network_id: 3,
-      gas: 4000000    //make sure this gas allocation isn't over 4M, which is the max
+      gas: 3000000,    //make sure this gas allocation isn't over 4M, which is the max
+      gasPrice: 100000000000
+      // optional config values:
+      // from - default address to use for any transaction Truffle makes during migrations
+      // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+      //          - function that returns a web3 provider instance (see below.)
+      //          - if specified, host and port are ignored.
     }
+  },
+  solc: {
+    //https://solidity.readthedocs.io/en/v0.4.24/using-the-compiler.html
+    optimizer: {
+      enabled: false,
+      runs: 200
+    }
+  },
+  mocha: {
+    useColors: true
   }
 };
