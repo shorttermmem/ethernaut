c2943bc6171a2d7194e388e2a6bc4f400e4cc1e2
rhuts <roman.h1234@gmail.com>
2018-12-12 09:46:31 -0500

    Update style for Telephone Test

--- test/telephone.test.js
+++ test/telephone.test.js
@@ -1,21 +1,24 @@
-var Telephone = artifacts.require("Telephone");
-var TelephoneHack = artifacts.require("TelephoneHack");
+const Telephone = artifacts.require("Telephone");
+const TelephoneHack = artifacts.require("TelephoneHack");
 
 
 contract("Telephone", (accounts) => {
+    let telephone;
+    let telephoneHack;
+
+    before(async () => {
+        telephone = await Telephone.deployed();
+        telephoneHack = await TelephoneHack.deployed();
+    });
+
     it("user should not be owner", async () => {
-        let telephone = await Telephone.deployed();
         let owner = await telephone.owner.call();
-        
-        var originalOwner = accounts[0];
+        let originalOwner = accounts[0];
         expect(owner).to.equal(originalOwner);
     });
 
     it("attacker should be able to become owner", async () => {
-        let telephone = await Telephone.deployed();
-        let telephoneHack = await TelephoneHack.deployed();
         let user = accounts[1];
-        
         await telephoneHack.changeOwner(user);
         
         let owner = await telephone.owner.call();
